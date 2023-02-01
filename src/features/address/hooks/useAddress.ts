6ec0dea78a90b0c391d17ac0d0b97fb279cf1c79
fl-yanai <yanai@framelunch.jp>
2023-02-01 15:53:05 +0900

    update

--- src/features/address/hooks/useAddress.ts
+++ src/features/address/hooks/useAddress.ts
@@ -5,19 +5,23 @@ import { TypeAddress } from '@/types/typeAddresses'
 export const useAddress = () => {
   const [address, setAddress] = useState('')
   const [loading, setLoading] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('')
+  const [error, setError] = useState(false)
 
   const searchAddress = async (zipcode: string) => {
     setLoading(true)
     let a: TypeAddress
     try {
       a = await getAddress(zipcode)
-      setAddress(`${a.results[0].address1}${a.results[0].address2}${a.results[0].address3}`)
+      setAddress(
+        `${a.results[0].address1}${a.results[0].address2}${
+          a.results[0].address3 ? a.results[0].address3 : ''
+        }`,
+      )
     } catch (err) {
-      setErrorMessage('connect error')
+      setError(true)
     }
     setLoading(false)
   }
 
-  return { address, loading, errorMessage, searchAddress }
+  return { address, loading, error, searchAddress }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,16 @@ import { useEffect } from 'react'
 import { useAddress } from '@/features/address/hooks/useAddress'
 
 const Home = () => {
-  const { address, loading, errorMessage, searchAddress } = useAddress()
+  const { address, loading, error, searchAddress } = useAddress()
   useEffect(() => {
     searchAddress('5140007')
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-  return <>{loading ? <p>取得中</p> : <p>{address}</p>}</>
+  return (
+    <>
+      {error && <p>エラーが発生しました</p>}
+      {loading ? <p>取得中</p> : <p>{address}</p>}
+    </>
+  )
 }
 export default Home
